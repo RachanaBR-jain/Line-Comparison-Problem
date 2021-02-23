93204f6522a17f944b5a4f87a450b3e475bde3a7
RachanaBR <brrachanajain@gmail.com>
2021-02-23 21:16:08 +0530

    added inbuild comapreTo function Program

--- LineComparsionProgram/LineComparsion.java
+++ LineComparsionProgram/LineComparsion.java
@@ -1,18 +1,28 @@
-//Welcome to LineComparsion Program
+package com.JavaPractiveProgram;
 import java.util.Random;
 public class LineComparsion {
 
-    public boolean Linear(int x1, int y1,int x2,int y2,int x3,int y3)
+    public int Linear(int x1, int y1,int x2,int y2,int x3,int y3)
     {
     	Integer distance1,distance2;
     	distance1=(int)Math.floor(Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)));
     	distance2=(int)Math.floor(Math.sqrt((x3-x1)*(x3-x1) + (y3-y1)*(y3-y1)));
     	
-	System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")="+distance1);
-	System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x3+","+y3+")="+distance2);
-        System.out.println("Distance b/w two line is equal (true/flase)");
+System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")="+distance1);
         
-    	boolean  res=distance1.equals(distance2); 
+System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x3+","+y3+")="+distance2);
+        
+    	int  res=distance1.compareTo(distance2);
+    	if(res==-1)
+    	{
+    		System.out.println("distance1 < distance2");
+    	}
+    	else if(res==1)
+    	{
+    		System.out.println("distance1  > distance 2");
+    	}
+    	else
+    		System.out.println("distance1 == distance 2");
     	return res;
     }
     public static void main(String[] args) {
@@ -28,5 +38,4 @@ public class LineComparsion {
 	}
 
 }
-
 
